ae0ba8bef797b901d6723fb0ac587bed4b35d780
Robin Holt <holt@sgi.com>
2013-07-08 16:01:40 -0700

    reboot: arm: change reboot_mode to use enum reboot_mode
    Preparing to move the parsing of reboot= to generic kernel code forces
    the change in reboot_mode handling to use the enum.
    
    Change-Id: I3b1ba671502286102732a5fd711b8c4bf5e03cba
    [akpm@linux-foundation.org: fix arch/arm/mach-socfpga/socfpga.c]
    Signed-off-by: Robin Holt <holt@sgi.com>
    Cc: Russell King <rmk+kernel@arm.linux.org.uk>
    Cc: Russ Anderson <rja@sgi.com>
    Cc: Robin Holt <holt@sgi.com>
    Cc: H. Peter Anvin <hpa@zytor.com>
    Cc: Guan Xuetao <gxt@mprc.pku.edu.cn>
    Acked-by: Russell King <rmk+kernel@arm.linux.org.uk>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Varun Wadekar <vwadekar@nvidia.com>
    Reviewed-on: http://git-master/r/603282
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>

--- arch/arm/mach-tegra/board.h
+++ arch/arm/mach-tegra/board.h
@@ -30,6 +30,7 @@
 #include <linux/memory.h>
 
 #include <mach/tegra_smmu.h>
+#include <linux/reboot.h>
 
 /*
  * OF is always used on ARM64
@@ -94,7 +95,7 @@
 
 struct memory_accessor;
 
-void tegra_assert_system_reset(char mode, const char *cmd);
+void tegra_assert_system_reset(enum reboot_mode mode, const char *cmd);
 
 void __init tegra20_init_early(void);
 void __init tegra30_init_early(void);
