a314da06194cb5bd7974c4d47c11aed43c3b05e6
Vartom <babanakov_dima@mail.ru>
2016-06-22 22:30:13 +0700

    fix Wunused-variable hotplug-denver

--- arch/arm64/mach-tegra/hotplug-denver.c
+++ arch/arm64/mach-tegra/hotplug-denver.c
@@ -53,13 +53,14 @@ void tegra_cpu_die(unsigned int cpu)
 		/* Synchronize with CPU0 */
 		while (secondary_holding_pen_release != cpu)
 			cpu_relax();
-	}
-	else {
+	} else {
+		pmstate = T132_CORE_C6;
+
 		do {
 			asm volatile (
 			"	msr actlr_el1, %0\n"
 			"	wfi\n"
-			: : "r" (T132_CORE_C6));
+			: : "r" (pmstate));
 		} while (secondary_holding_pen_release != cpu);
 	}
 
