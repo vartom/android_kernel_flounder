ec014631f0c86ca5b80f7b015ae82db025da12c4
Vartom <babanakov_dima@mail.ru>
2017-04-28 15:15:41 +0700

    Sound add

---
+++ include/linux/sound_on.h
@@ -0,0 +1,15 @@
+/*
+ * include/linux/headset_on.h
+ *
+ * Copyright (c) 2016-2017 Vartom
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+
+bool is_mdmhp_on(void);
+bool is_mdmsp_on(void);
+
+
