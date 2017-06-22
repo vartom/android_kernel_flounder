f4eaf907679ceb596446cc3d9dd8dee112f553f8
Vartom <babanakov_dima@mail.ru>
2017-06-22 18:11:39 +0700

    Revert "PASR: mm: Integrate PASR in Buddy allocator"
    This reverts commit ccc5d8f1fc45d4729e7fcb777d2644e3196f3511.

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -56,7 +56,6 @@
 #include <linux/ftrace_event.h>
 #include <linux/memcontrol.h>
 #include <linux/prefetch.h>
-#include <linux/pasr.h>
 #include <linux/migrate.h>
 #include <linux/page-debug-flags.h>
 #include <linux/hugetlb.h>
@@ -575,7 +574,6 @@ static inline void __free_one_page(struct page *page,
 		} else {
 			list_del(&buddy->lru);
 			zone->free_area[order].nr_free--;
-			pasr_kget(buddy, order);
 			rmv_page_order(buddy);
 		}
 		combined_idx = buddy_idx & page_idx;
@@ -609,7 +607,6 @@ static inline void __free_one_page(struct page *page,
 	list_add(&page->lru, &zone->free_area[order].free_list[migratetype]);
 out:
 	zone->free_area[order].nr_free++;
-	pasr_kput(page, order);
 }
 
 static inline int free_pages_check(struct page *page)
@@ -843,7 +840,6 @@ static inline void expand(struct zone *zone, struct page *page,
 #endif
 		list_add(&page[size].lru, &area->free_list[migratetype]);
 		area->nr_free++;
-		pasr_kput(page, high);
 		set_page_order(&page[size], high);
 	}
 }
@@ -912,7 +908,6 @@ struct page *__rmqueue_smallest(struct zone *zone, unsigned int order,
 		list_del(&page->lru);
 		rmv_page_order(page);
 		area->nr_free--;
-		pasr_kget(page, current_order);
 		expand(zone, page, order, current_order, area, migratetype);
 		set_freepage_migratetype(page, migratetype);
 		return page;
@@ -1097,7 +1092,6 @@ __rmqueue_fallback(struct zone *zone, unsigned int order, int start_migratetype)
 			page = list_entry(area->free_list[migratetype].next,
 					struct page, lru);
 			area->nr_free--;
-			pasr_kget(page, current_order);
 
 			if (!is_migrate_cma(migratetype)) {
 				try_to_steal_freepages(zone, page,
@@ -1467,8 +1461,6 @@ int __isolate_free_page(struct page *page, unsigned int order)
 	/* Remove page from free list */
 	list_del(&page->lru);
 	zone->free_area[order].nr_free--;
-	pasr_kget(page, order);
-
 	rmv_page_order(page);
 
 	/* Set the pageblock if the isolated page is at least a pageblock */
@@ -6203,7 +6195,6 @@ __offline_isolated_pages(unsigned long start_pfn, unsigned long end_pfn)
 		list_del(&page->lru);
 		rmv_page_order(page);
 		zone->free_area[order].nr_free--;
-		pasr_kget(page, order);
 #ifdef CONFIG_HIGHMEM
 		if (PageHighMem(page))
 			totalhigh_pages -= 1 << order;
