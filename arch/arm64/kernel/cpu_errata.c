defdb2dbabb7aafb23ed52e0869d220ec70992da
Vartom <babanakov_dima@mail.ru>
2017-08-27 22:11:31 +0700

    Revert "arm64: fix midr range for Cortex-A57 erratum 832075"
    This reverts commit 6b13d7b77c9119261a1cdcfcee5bb984d1b028dc.

--- arch/arm64/kernel/cpu_errata.c
+++ arch/arm64/kernel/cpu_errata.c
@@ -88,8 +88,7 @@ struct arm64_cpu_capabilities arm64_errata[] = {
 	/* Cortex-A57 r0p0 - r1p2 */
 		.desc = "ARM erratum 832075",
 		.capability = ARM64_WORKAROUND_DEVICE_LOAD_ACQUIRE,
-		MIDR_RANGE(MIDR_CORTEX_A57, 0x00,
-			   (1 << MIDR_VARIANT_SHIFT) | 2),
+		MIDR_RANGE(MIDR_CORTEX_A57, 0x00, 0x12),
 	},
 #endif
 	{
