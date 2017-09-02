cda14b5fa27cfa17a248a47f9dbc2311ecdd9045
Vartom <babanakov_dima@mail.ru>
2017-09-02 12:51:13 +0700

    Revert "arm64: skip cpu nodes marked as disabled in the device  tree."
    This reverts commit 438a2c61f3ca17d620a00aaaba8bc75e42666284.

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -390,12 +390,6 @@ void __init smp_init_cpus(void)
 		}
 
 		/*
-		 * Check to see if the cpu is disabled.
-		 */
-		if (!of_device_is_available(dn))
-			goto next;
-
-		/*
 		 * Duplicate MPIDRs are a recipe for disaster. Scan
 		 * all initialized entries and check for
 		 * duplicates. If any is found just ignore the cpu.
